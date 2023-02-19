6685a4127217bcb1d289580bfa6f6c6d9b47ba0d
Elliot Bobrow <elliotgreybobrow@gmail.com>
2023-02-18 20:30:48 -0800

    weird and kind of pointless benchmark!

---
+++ racket/bench.rkt
@@ -0,0 +1,30 @@
+#!/usr/bin/racket
+
+#lang racket
+
+(require "mashhap.rkt" benchmark plot)
+
+(define results
+  (run-benchmarks
+   (list 'bench)
+   (list (range 10 1000 10))
+   (lambda (_bench len)
+     (define map (mashhap-new 5))
+     ; TODO: this could be a macro in the main file couldn't it
+     (define (set!-map map k v)
+       (set! map (mashhap-set map k v)))
+     (for [(i (in-range len))]
+       (set!-map map (make-string i) i)))
+   #:extract-time 'delta-time))
+
+(plot-new-window? #t)
+
+(parameterize ([plot-x-ticks no-ticks])
+  (plot
+   #:title "bench"
+   #:x-label "size"
+   #:y-label "normalized time"
+   (render-benchmark-alts
+    ; default options
+    (list 10)
+    results)))
